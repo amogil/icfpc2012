c8dd503972d43c7f7530dc0d9561ecde53f282a2
Alexey Mogilnikov <alexey@mogilnikov.name>
2012-07-13 22:05:28 +0600

    Рыба генератора карт

---
+++ icfpc2012/MapGenerator/Generator.cs
@@ -0,0 +1,37 @@
+﻿using Logic;
+
+namespace MapGenerator
+{
+	internal class Generator
+	{
+		public Generator(int height, int width)
+		{
+			Height = height;
+			Width = width;
+		}
+
+		private int Height { get; set; }
+		private int Width { get; set; }
+
+		public string Generate()
+		{
+			var map = new MapCell[Width,Height];
+			PutWalls(map);
+			return "";
+		}
+
+		private void PutWalls(MapCell[,] map)
+		{
+			for(int i = 0; i < Width; i++)
+			{
+				map[i, 0] = MapCell.Wall;
+				map[i, Height - 1] = MapCell.Wall;
+			}
+			for(int j = 0; j < Height; j++)
+			{
+				map[0, j] = MapCell.Wall;
+				map[Width - 1, j] = MapCell.Wall;
+			}
+		}
+	}
+}
