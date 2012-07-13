5b0856e6570c63e197bbc9f8b328f4b956c0b41c
Pavel Egorov <xoposhiy@mail.ru>
2012-07-13 18:53:57 +0600

    Map interface

--- icfpc2012/Logic/Map.cs
+++ icfpc2012/Logic/Map.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Logic
 {
+	public enum RobotMove
+	{
+		Left,
+		Right,
+		Up,
+		Down,
+		Wait
+	}
+	
+	public enum MapCell
+	{
+		Empty,
+		Earth,
+		Rock,
+		Lambda,
+		Robot,
+		Lift,
+		OpenedLift
+	}
+
 	public class Map
 	{
-		
+		private MapCell[,] map;
+
+		public Map(string[] lines)
+		{
+			throw new NotImplementedException();
+		}
+
+		public MapCell this[int x, int y]
+		{
+			get { return map[x, y]; }
+		}
+
+		public Map Move(RobotMove move)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
