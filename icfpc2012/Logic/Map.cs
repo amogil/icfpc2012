8d0117b1a73c302d200052aefc1eaae72e2fda54
unknown <andrew@nv-ur-150.kontur>
2012-07-13 19:28:18 +0600

    Added map reader. Fixed little stuffs with enums. Added move maker interface.

--- icfpc2012/Logic/Map.cs
+++ icfpc2012/Logic/Map.cs
@@ -8,7 +8,8 @@ namespace Logic
 		Right,
 		Up,
 		Down,
-		Wait
+		Wait,
+        Abort
 	}
 	
 	public enum MapCell
@@ -17,28 +18,88 @@ namespace Logic
 		Earth,
 		Rock,
 		Lambda,
+        Wall,
 		Robot,
-		Lift,
+		ClosedLift,
 		OpenedLift
 	}
 
+    public enum MoveResult
+    {
+        Valid,
+        Invalid
+    }
+
+    public enum CheckResult
+    {
+        Nothing,
+        Win,
+        Fail
+    }
+
 	public class Map
-	{
-		private MapCell[,] map;
+    {
+        readonly public int Width;
+        readonly public int Height;
+        private MapCell[,] CurrentMap;
+
+        public int RobotX { get; private set; }
+        public int RobotY { get; private set; }
 
 		public Map(string[] lines)
 		{
-			throw new NotImplementedException();
+		    Height = lines.Length;
+		    Width = lines[0].Length;
+
+            for (int i = 0; i < Width; i++ )
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    CurrentMap[i, j] = Parse(lines[i][j]);
+                    if (CurrentMap[i, j] == MapCell.Robot)
+                    {
+                        RobotX = i;
+                        RobotY = j;
+                    }
+                }
+            }
 		}
 
-		public MapCell this[int x, int y]
+        private static MapCell Parse(char c)
+        {
+            switch (c)
+            {
+                case '#': return MapCell.Wall;
+                case '*': return MapCell.Rock;
+                case '/': return MapCell.Lambda;
+                case '.': return MapCell.Earth;
+                case ' ': return MapCell.Empty;
+                case 'L': return MapCell.ClosedLift;
+                case 'O': return MapCell.OpenedLift;
+                case 'R': return MapCell.Robot;
+            }
+
+            throw new Exception("InvalidMap");
+        }
+
+	    public MapCell this[int x, int y]
 		{
-			get { return map[x, y]; }
+			get { return CurrentMap[x, y]; }
 		}
 
-		public Map Move(RobotMove move)
-		{
-			throw new NotImplementedException();
-		}
+        public MoveResult Move(RobotMove move)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CheckResult Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Map Update()
+        {
+            throw new NotImplementedException();
+        }
 	}
 }
