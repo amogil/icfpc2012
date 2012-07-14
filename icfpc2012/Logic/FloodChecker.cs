656a6aca4e51a6d10c3a099f85c14b29c813f530
Michael <andrew@nv-ur-150.kontur>
2012-07-14 18:44:17 +0600

    Added waterproof counter

---
+++ icfpc2012/Logic/FloodChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+	static class FloodChecker
+	{
+		static int GetLevel(int water, int flooding, int time)
+		{
+			return water + time/flooding;
+		}
+	}
+}
