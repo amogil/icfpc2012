2d61c737f95e9eb262477e2261a9d8b4efe0c76a
Michael <andrew@nv-ur-150.kontur>
2012-07-14 17:02:28 +0600

    added some tests

--- icfpc2012/Logic/Map.cs
+++ icfpc2012/Logic/Map.cs
@@ -328,7 +328,7 @@ namespace Logic
 
 		public bool IsSafeMove(Vector from, Vector to)
 		{
-			if (from.Y + 2 > Height)
+			if (to.Y + 2 > Height)
 				return true;
 
 			bool isSafe = true;
@@ -525,8 +525,11 @@ namespace Logic
 		public void Test()
 		{
 			Map contest1 = WellKnownMaps.Contest1();
+			Map contest4 = WellKnownMaps.Contest4();
 			Assert.IsTrue(contest1.IsSafeMove(new Vector(5, 5), new Vector(5, 4)));
 			Assert.IsFalse(contest1.IsSafeMove(new Vector(5, 4), new Vector(5, 3)));
+			Assert.IsFalse(contest4.IsSafeMove(new Vector(3, 7), new Vector(3, 6)));
+			Assert.IsTrue(contest4.IsSafeMove(new Vector(3, 6), new Vector(3, 5)));
 		}
 	}
 }
