5523af3c5c75c3f1ea83b7643b7804a7a47784d0
Pavel Egorov <xoposhiy@mail.ru>
2012-07-14 18:26:34 +0600

    solution.tgz to submit

--- icfpc2012/Logic/Map.cs
+++ icfpc2012/Logic/Map.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using NUnit.Framework;
 
 namespace Logic
 {
@@ -532,18 +531,8 @@ namespace Logic
 	{
 	}
 
-	[TestFixture]
-	public class MapIsSafeMove_Test
+	public class KilledByRockException : GameFinishedException
 	{
-		[Test]
-		public void Test()
-		{
-			Map contest1 = WellKnownMaps.Contest1();
-			Map contest4 = WellKnownMaps.Contest4();
-			Assert.IsTrue(contest1.IsSafeMove(new Vector(5, 5), new Vector(5, 4)));
-			Assert.IsFalse(contest1.IsSafeMove(new Vector(5, 4), new Vector(5, 3)));
-			Assert.IsFalse(contest4.IsSafeMove(new Vector(3, 7), new Vector(3, 6)));
-			Assert.IsTrue(contest4.IsSafeMove(new Vector(3, 6), new Vector(3, 5)));
-		}
 	}
+
 }
